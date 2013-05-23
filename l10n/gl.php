1e519bab638b63dbda5581d5a2b9f0f609000139
Antón Méixome <meixome@certima.net>
2013-05-23 20:17:25 +0300

    Please, add galician translation
    Hi, 
    I think this may be the method for contributing with a translation

---
+++ l10n/gl.php
@@ -0,0 +1,32 @@
+<?php $TRANSLATIONS = array(
+    
+    "Messages" => "Mensaxes" ,   
+    "Write Message" => "Redactar mensaxe" ,
+    "Search" => "Buscar" ,
+    "The user photo application also need to be enabled to use this application." => "Para usar este aplicativo debe estar activado o aplicativo user photo." ,
+    "Message to ..." => "Mensaxe para ..." ,
+    "content ..." => "contido ..." ,
+    "Send Message" => "Enviar mensaxe" ,
+    "The message was removed." => "Eliminouse a mensaxe.",
+    "Error - Internal Message" => "Erro - Mensaxes internas" ,
+    "Success - Internal Message" => "Aceptado - Mensaxes internas" ,
+    "The message has been sent." => "Enviouse a mensaxe." ,
+    "error when trying to sent the message.\ncheck the sender and the content." => "produciuse un erro ao tentar enviar a mensaxe.\ncomprobe o destinatario e o contido." ,
+    "error when trying to sent the message.\nall fields must be filled." => "produciuse un erro ao tentar enviar a mensaxe.\ndeben de cubrirse todos os campos." ,
+    "error when trying search the message." => "produciuse un erro ao tentar buscar a mensaxe." ,
+
+    "the file " => "o ficheiro ",
+    "the folder " => "o cartafol ",
+    "I shared with you " => "Compartino contigo " ,
+
+    "Send Mail" => "Enviar correo" ,
+    "Send mail with the contents of the message" => "Enviar correo co contido da mensaxe" ,
+
+    "Reply" => "Responder" ,
+    "Delete" => "Eliminar" ,
+    "Quit" => "Quitar" ,
+
+    "Read in" => "Leído o " ,
+    "You can format your text with BBCode" => "Pode formatar o seu texto en formato BBCode" ,
+    "Help" => "Axuda" ,
+);
