6b38080e8e74b18c259f9d1c404856dc285b93b6
Jorge Rafael Garcia Ramos <kadukeitor@gmail.com>
2013-05-23 14:32:15 -0400

    spanish translation

---
+++ l10n/es.php
@@ -0,0 +1,32 @@
+<?php $TRANSLATIONS = array(
+    
+    "Messages" => "Mensajes" ,   
+    "Write Message" => "Redactar Mensaje" ,
+    "Search" => "Buscar" ,
+    "The user photo application also need to be enabled to use this application." => "La aplicación user photo debe estar habilitada." ,
+    "Message to ..." => "Mensaje a ..." ,
+    "content ..." => "contenido ..." ,
+    "Send Message" => "Enviar Mensaje" ,
+    "The message was removed." => "El Mensaje fue eliminado.",
+    "Error - Internal Message" => "Error - Mensajes Internos" ,
+    "Success - Internal Message" => "Ok - Mensajes Internos" ,
+    "The message has been sent." => "El Mensaje fue enviado." ,
+    "error when trying to sent the message.\ncheck the sender and the content." => "error cuando se intentaba enviar el mensaje.\nchequee el destinatario y el contenido." ,
+    "error when trying to sent the message.\nall fields must be filled." => "error cuando se intentaba enviar el mensaje.\ntodos los campos deben ser llenados." ,
+    "error when trying search the message." => "error cuando se intentaba buscar el mensaje." ,
+
+    "the file " => "el archivo ",
+    "the folder " => "la carpeta ",
+    "I shared with you " => "Te he compartido " ,
+
+    "Send Mail" => "Enviar Correo" ,
+    "Send mail with the contents of the message" => "Enviar correo con el contenido del mensaje" ,
+
+    "Reply" => "Responder" ,
+    "Delete" => "Eliminar" ,
+    "Quit" => "Quitar" ,
+
+    "Read in" => "Leído el" ,
+    "You can format your text with BBCode" => "Usted puede formatear su texto en formato BBCode" ,
+    "Help" => "Ayuda" ,
+);
